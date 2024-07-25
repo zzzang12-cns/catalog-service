5ba6d1c7d1795db5457ce0208808b72bfa12030a
zzzang12 <wadevs12@gmail.com>
2024-07-26 02:29:23 +0900

    Data persistence with Spring Data JDBC

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -4,8 +4,17 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+
+import java.time.Instant;
 
 public record Book(
+  @Id
+  Long id,
+
   @NotBlank(message = "The book ISBN must be defined.")
   @Pattern(
     regexp = "^([0-9]{10}|[0-9]{13})$",
@@ -21,6 +30,18 @@ public record Book(
 
   @NotNull(message = "The book price must be defined.")
   @Positive(message = "The book price must be greater than zero.")
-  Double price
+  Double price,
+
+  @CreatedDate
+  Instant createdDate,
+
+  @LastModifiedDate
+  Instant lastModifiedDate,
+
+  @Version
+  int version
 ) {
+  public static Book of(String isbn, String title, String author, Double price) {
+    return new Book(null, isbn, title, author, price, null, null, 0);
+  }
 }
--- src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -32,10 +32,14 @@ public class BookService {
   public Book editBookDetails(String isbn, Book book) {
     return bookRepository.findByIsbn(isbn).map(existingBook -> {
       var bookToUpdate = new Book(
+        existingBook.id(),
         existingBook.isbn(),
         book.title(),
         book.author(),
-        book.price()
+        book.price(),
+        existingBook.createdDate(),
+        existingBook.lastModifiedDate(),
+        existingBook.version()
       );
       return bookRepository.save(bookToUpdate);
     }).orElseGet(() -> addBookToCatalog(book));
